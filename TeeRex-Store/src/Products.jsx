0d5f92586331d0bc40a613e792e399108fcc2521
Praveen Katti <praveendk25@gmail.com>
2025-03-05 12:15:16 +0530

    added scale and hover to produtcs and edited shadow property

--- TeeRex-Store/src/Products.jsx
+++ TeeRex-Store/src/Products.jsx
@@ -20,11 +20,11 @@ export default function Products(){
     },[])
 
     return (
-        <div className="flex flex-wrap gap-4">
+        <div className="flex flex-wrap gap-7">
             {
                 product.map((item) => (
-                    <div key={item.id} className="w-[32%] rounded p-4 shadow-[0px_0px_5px_1px_rgba(0,0,0,0.1)]">
-                        <div><img src={item.imageURL} alt={item.name} className="w-full h-40 object-cover"/></div>
+                    <div key={item.id} className="w-[30%] rounded p-4 shadow-[0px_0px_4px_0.5px_rgba(0,0,0,0.1)]">
+                        <div><img src={item.imageURL} alt={item.name} className="scale-85 hover:scale-100 w-full h-40 object-cover"/></div>
                         <div className="flex items-center mt-3 mx-1 w-[full] justify-between">
                             <p className="font-bold">Rs {item.price}</p>
                             <button className=
