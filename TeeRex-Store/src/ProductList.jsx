b15186a7931963aac9f5426235ec385ef0a82228
Praveen Katti <praveendk25@gmail.com>
2025-03-06 15:38:09 +0530

    created states in ProductList for input queries and passed prop to Products, added new variable displayTerm to Products to handle both default and filtered Products

--- TeeRex-Store/src/ProductList.jsx
+++ TeeRex-Store/src/ProductList.jsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
 import Filter from './Filter'
 import Products from './Products'
 import { PiMagnifyingGlassBold } from "react-icons/pi";
 
 export default function ProductList({addToCart}) {
+    const [inputTerm, setInputTerm] = useState("")
+    const [searchTerm, setSearchTerm] = useState("")
+
     return (
         <div>
             <div className='flex justify-between mx-10 mt-5'>
                 <p className='invisible'>filtered results</p>
                 <div className='flex items-center gap-5'>
-                    <input type="search" placeholder='Search for products...' 
-                    className='border-b-2 py-2 border-gray-300 focus:outline-none w-[300px]'/>
-                    <span className='bg-gray-400 py-1 px-4 flex-shrink-0 rounded hover:bg-gray-600 cursor-pointer'>
+                    <input 
+                        type="search" 
+                        placeholder='Search for products...'
+                        className='border-b-2 py-2 border-gray-300 focus:outline-none w-[300px]'
+                        value={inputTerm}
+                        onChange={(e)=>setInputTerm(e.target.value)}
+                    />
+                    <span
+                        onClick={()=>setSearchTerm(inputTerm)} 
+                        className='bg-gray-400 py-1 px-4 flex-shrink-0 rounded hover:bg-gray-600 cursor-pointer'
+                    >
                         <PiMagnifyingGlassBold className='text-white text-2xl'/>
                     </span>
                 </div>
@@ -25,7 +37,7 @@ export default function ProductList({addToCart}) {
                     <Filter />
                 </div>
                 <div className="w-3/4">
-                    <Products addToCart={addToCart} />
+                    <Products searchTerm={searchTerm} addToCart={addToCart} />
                 </div>
             </div>
 
--- TeeRex-Store/src/Products.jsx
+++ TeeRex-Store/src/Products.jsx
@@ -1,6 +1,6 @@
 import { useEffect,useState } from "react"
 
-export default function Products({addToCart}){
+export default function Products({searchTerm,addToCart}){
     const [product, setProduct] = useState([])
 
     useEffect(()=>{
@@ -9,9 +9,10 @@ export default function Products({addToCart}){
                 const data = await fetch('https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json');
                 if (!data.ok) {
                     throw new Error("Error occurred: cannot fetch data");
+                }else{
+                    const products = await data.json();
+                    setProduct(products);
                 }
-                const products = await data.json();
-                setProduct(products);
             } catch(error) {
                 console.error(error);
             }
@@ -19,11 +20,18 @@ export default function Products({addToCart}){
         getProduct()
     },[])
 
+    const displayProducts = searchTerm
+        ? product.filter((item) =>
+              item.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+        : product;
+
     return (
         <div className="flex flex-wrap gap-7">
             {
-                product.map((item) => (
+                displayProducts.map((item) => (
                     <div key={item.id} className="w-[30%] rounded p-4 shadow-[0px_0px_4px_0.5px_rgba(0,0,0,0.1)]">
+                        <div className="mb-5 font-bold"><p>{item.name}</p></div>
                         <div><img src={item.imageURL} alt={item.name} className="scale-85 hover:scale-100 w-full h-40 object-cover"/></div>
                         <div className="flex items-center mt-3 mx-1 w-[full] justify-between">
                             <p className="font-bold">Rs {item.price}</p>
