3d1ed92b7c2fdb91bc8cd738f82d1751533ed2a4
Praveen Katti <praveendk25@gmail.com>
2025-03-06 21:23:48 +0530

    added live link for website in readme and changed color of button AddToCart added hover effect

--- TeeRex-Store/src/Products.jsx
+++ TeeRex-Store/src/Products.jsx
@@ -32,11 +32,17 @@ export default function Products({searchTerm,addToCart}){
                 displayProducts.map((item) => (
                     <div key={item.id} className="w-[30%] rounded p-4 shadow-[0px_0px_4px_0.5px_rgba(0,0,0,0.1)]">
                         <div className="mb-5 font-bold"><p>{item.name}</p></div>
-                        <div><img src={item.imageURL} alt={item.name} className="scale-85 hover:scale-100 w-full h-40 object-cover"/></div>
+                        <div>
+                            <img 
+                                src={item.imageURL} 
+                                alt={item.name} 
+                                className="scale-85 hover:scale-100 w-full h-40 object-cover"
+                            />
+                        </div>
                         <div className="flex items-center mt-3 mx-1 w-[full] justify-between">
                             <p className="font-bold">Rs {item.price}</p>
                             <button onClick={()=>addToCart(item)} className=
-                            "p-2 bg-black text-sm text-white w-[max] rounded cursor-pointer">
+                            "p-2 bg-gray-500 hover:bg-black text-sm text-white w-[max] rounded cursor-pointer">
                             Add to Cart</button>
                         </div>
                     </div>
