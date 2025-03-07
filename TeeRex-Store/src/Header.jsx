12b1ee36f92a7fe8b3d7175e506ae31fe63d3ace
Praveen Katti <praveendk25@gmail.com>
2025-03-07 18:39:13 +0530

    made header component responsive

--- TeeRex-Store/src/Header.jsx
+++ TeeRex-Store/src/Header.jsx
@@ -2,20 +2,20 @@ import { IoCartOutline } from "react-icons/io5";
 import { Link } from 'react-router-dom'
 export default function Header({cart}) {
     return (
-        <div className="flex justify-between py-5 px-10 bg-gray-200 z-50 sticky w-full top-0">
+        <div className="flex justify-between p-5 md:py-5 md:px-10 bg-gray-200 z-50 sticky w-full top-0">
             <Link to={'/'}>
-                <div><h1 className="text-2xl">TeeRex Store</h1></div>
+                <div><h1 className="text-[1.3rem] font-bold md:text-2xl">TeeRex Store</h1></div>
             </Link>
             
             {/* Navigates to Home page*/}
             <div className="flex gap-3">
                 <Link to='/'>
-                    <p className="text-xl hover:text-teal-700">Products</p>
+                    <p className="text-[1.1rem] md:text-xl hover:text-teal-700">Products</p>
                 </Link>
                 
                 {/* Navigates to cart on clicking cart icon*/}
                 <Link to='Cart'>
-                    <div className="flex text-4xl hover:text-teal-700">
+                    <div className="flex text-3xl md:text-4xl hover:text-teal-700">
                         <IoCartOutline />
                         <sup className="text-sm font-bold">{cart}</sup>
                     </div>
