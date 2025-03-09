72681fb55a36de064136f037bb7af968c9c22e76
Praveen Katti <praveendk25@gmail.com>
2025-03-09 18:01:02 +0530

    Created new Component for Search and updated ProductList, added message if not matches for search results in Product component

--- TeeRex-Store/src/ProductList.jsx
+++ TeeRex-Store/src/ProductList.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import Filter from './Filter'
 import Products from './Products'
-import { PiMagnifyingGlassBold } from "react-icons/pi";
-import { FiFilter } from "react-icons/fi";
+import Search from './Search';
 
 export default function ProductList({ addToCart }) {
-    const [inputTerm, setInputTerm] = useState("")
     const [searchTerm, setSearchTerm] = useState("")
     const [toggleFilter, setToggleFilter] = useState(new Set(["hidden", "md:hidden", "absolute", "right-0", "top-0", "z-40"]))
 
+    function searchQuery(query){
+        setSearchTerm(query)
+    }
     function updateFilter() {
         const newToggleFilter = new Set(toggleFilter)
         newToggleFilter.has("hidden") ? newToggleFilter.delete("hidden") : newToggleFilter.add("hidden")
@@ -19,25 +20,10 @@ export default function ProductList({ addToCart }) {
 
     return (
         <div>
-            <div className='flex justify-center mx-[2%] mt-[4%] lg:mx-10 lg:mt-5'>
-                <div className='flex items-center gap-2 lg:gap-5'>
-                    <input
-                        type="search"
-                        placeholder='Search for products...'
-                        className='border-b-2 py-1 lg:py-2 border-gray-300 focus:outline-none w-[65vw] sm:w-[30vw]'
-                        value={inputTerm}
-                        onChange={(e) => setInputTerm(e.target.value)}
-                    />
-                    <span
-                        onClick={() => setSearchTerm(inputTerm)}
-                        className='bg-gray-400 py-1 px-3 lg:px-4 flex-shrink-0 rounded hover:bg-gray-600 cursor-pointer'
-                    >
-                        <PiMagnifyingGlassBold className='text-white text-xl' />
-                    </span>
-                    <span onClick={updateFilter} className='md:hidden bg-gray-400 py-1 px-3 lg:px-4 flex-shrink-0 rounded hover:bg-gray-600 cursor-pointer'>
-                        <FiFilter className='text-white text-xl' />
-                    </span>
-                </div>
+            <div>
+                <Search 
+                    updateFilter={updateFilter} 
+                    searchQuery={searchQuery}/>
             </div>
             <div className='relative'>
                 <div className={filterClasses}>
--- TeeRex-Store/src/Products.jsx
+++ TeeRex-Store/src/Products.jsx
@@ -26,28 +26,34 @@ export default function Products({searchTerm,addToCart}){
           )
         : product;
 
-    return (
-        <div className="grid gap-5 md:flex md:flex-wrap md:gap-7">
-            {
-                displayProducts.map((item) => (
-                    <div key={item.id} className="w-full md:w-[30%] rounded p-4 shadow-[0px_0px_4px_0.5px_rgba(0,0,0,0.1)]">
-                        <div className="mb-1 md:mb-5 font-bold"><p>{item.name}</p></div>
-                        <div>
-                            <img 
-                                src={item.imageURL} 
-                                alt={item.name} 
-                                className="scale-85 hover:scale-100 w-full h-40 object-cover"
-                            />
+    if(displayProducts && displayProducts.length!==0){
+        return (
+            <div className="grid gap-5 md:flex md:flex-wrap md:gap-7">
+                {
+                    displayProducts.map((item) => (
+                        <div key={item.id} className="w-full md:w-[30%] rounded p-4 shadow-[0px_0px_4px_0.5px_rgba(0,0,0,0.1)]">
+                            <div className="mb-1 md:mb-5 font-bold"><p>{item.name}</p></div>
+                            <div>
+                                <img 
+                                    src={item.imageURL} 
+                                    alt={item.name} 
+                                    className="scale-85 hover:scale-100 w-full h-40 object-cover"
+                                />
+                            </div>
+                            <div className="flex items-center mt-3 mx-1 w-[full] justify-between">
+                                <p className="font-bold">Rs {item.price}</p>
+                                <button onClick={()=>addToCart(item)} className=
+                                "p-2 bg-gray-500 hover:bg-black text-sm text-white w-[max] rounded cursor-pointer">
+                                Add to Cart</button>
+                            </div>
                         </div>
-                        <div className="flex items-center mt-3 mx-1 w-[full] justify-between">
-                            <p className="font-bold">Rs {item.price}</p>
-                            <button onClick={()=>addToCart(item)} className=
-                            "p-2 bg-gray-500 hover:bg-black text-sm text-white w-[max] rounded cursor-pointer">
-                            Add to Cart</button>
-                        </div>
-                    </div>
-                ))
-            }
-        </div>
-    )
+                    ))
+                }
+            </div>
+        )
+    }else{
+        return(
+            <p className="font-bold">No matched Products!</p>
+        )
+    }
 }
