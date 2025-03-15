afb598dba2d458747e2459e8e9c4ae138c57105f
Praveen Katti <praveendk25@gmail.com>
2025-03-15 16:23:46 +0530

    made more responsive for medium screens

--- TeeRex-Store/src/Products.jsx
+++ TeeRex-Store/src/Products.jsx
@@ -8,19 +8,19 @@ export default function Products({productList,searchTerm,addToCart}){
 
     if(displayProducts && displayProducts.length!==0){
         return (
-            <div className="grid gap-5 md:flex md:flex-wrap md:gap-7">
+            <div className="grid gap-5 sm:flex sm:flex-wrap md:gap-10 lg:gap-7">
                 {
                     displayProducts.map((item) => (
-                        <div key={item.id} className="w-full md:w-[30%] rounded p-4 shadow-[0px_0px_4px_0.5px_rgba(0,0,0,0.1)]">
-                            <div className="mb-1 md:mb-5 font-bold"><p>{item.title}</p></div>
-                            <div>
+                        <div key={item.id} className="w-full sm:w-[48%] md:w-[45%] lg:w-[30%] rounded p-4 shadow-[0px_0px_4px_0.5px_rgba(0,0,0,0.1)]">
+                            <div className="mb-1 md:mb-2 font-bold"><p>{item.title}</p></div>
+                            <div className="w-full sm:h-80 md:h-75 lg:h-64">
                                 <img 
                                     src={item.image} 
                                     alt={item.title} 
-                                    className="scale-85 hover:scale-100 w-full h-40 object-cover"
+                                    className="scale-100 w-full h-full object-cover"
                                 />
                             </div>
-                            <div className="flex items-center mt-3 mx-1 w-[full] justify-between">
+                            <div className="flex items-center mt-2 mx-1 w-[full] justify-between">
                                 <p className="font-bold">Rs {item.price}</p>
                                 <button onClick={()=>addToCart(item)} className=
                                 "p-2 bg-gray-500 hover:bg-black text-sm text-white w-[max] rounded cursor-pointer">
--- TeeRex-Store/src/ShoppingCart.jsx
+++ TeeRex-Store/src/ShoppingCart.jsx
@@ -12,24 +12,24 @@ export default function Cart({ cart, quantity, getQuantity, removeFromCart }) {
 
 
     return (
-        <div className="w-[90vw] md:w-1/2 shadow-[0px_0px_4px_0.5px_rgba(0,0,0,0.1)] md:mx-10 md:mt-5 m-5 p-5">
+        <div className="w-[90vw] md:w-2/3 lg:w-1/2 shadow-[0px_0px_4px_0.5px_rgba(0,0,0,0.1)] md:mx-10 md:mt-5 m-5 p-5">
             <div className="text-xl md:text-2xl font-semibold"><p>Shopping Cart</p></div>
             <div className="mt-5 md:mt-10 md:ms-10 md:w-[90%]">
                 {
                     Array.from(cart.values()).map((item) => (
                         <div key={item.id} className="grid gap-5 md:flex p-5 shadow-[0px_0px_4px_0.5px_rgba(0,0,0,0.1)] mb-5">
                             <div className="flex gap-5 md:w-2/3">
-                                <img className="w-20 h-20" src={item.image} alt={item.title} />
+                                <img className="w-25 h-30 md:w-30 md:h-40" src={item.image} alt={item.title} />
                                 <div className="font-bold">
                                     <p>{item.title}</p>
                                     <p className="flex items-center"><MdCurrencyRupee className="inline-block"/>{item.price} * {quantity[item.id] || 1}</p>
                                 </div>
                             </div>
                             <div className="md:w-1/3 flex gap-5 justify-end md:items-end">
-                                <div className="flex gap-2 w-1/2">
-                                    <label className="text-center text-sm rounded bg-gray-200 cursor-pointer py-1 w-2/5" onClick={()=>getQuantity(-1,item)}>➖</label>
+                                <div className="flex gap-2 md:w-1/2">
+                                    <label className="text-center text-sm rounded bg-gray-200 cursor-pointer w-10 py-1 md:w-2/5" onClick={()=>getQuantity(-1,item)}>➖</label>
                                         <label>{quantity[item.id] || 1}</label>
-                                    <label className="text-center text-sm rounded bg-gray-200 cursor-pointer py-1 w-2/5" onClick={()=>getQuantity(1,item)}>➕</label>
+                                    <label className="text-center text-sm rounded bg-gray-200 cursor-pointer w-10 py-1 md:w-2/5" onClick={()=>getQuantity(1,item)}>➕</label>
                                 </div>
                                 <button onClick={() => removeFromCart(item)} className="rounded bg-gray-200 hover:bg-gray-300 cursor-pointer py-1 px-2">Delete</button>
                             </div>
