f466a915c05880e370a9e02c57afb8375e4d105b
Praveen Katti <praveendk25@gmail.com>
2025-12-04 11:47:17 +0530

    fixed image size of product card

--- TeeRex-Store/src/components/Products.jsx
+++ TeeRex-Store/src/components/Products.jsx
@@ -1,44 +1,42 @@
 import { useNavigate } from "react-router-dom";
-export default function Products({productList,searchTerm,addToCart}){
+export default function Products({ productList, searchTerm, addToCart }) {
     const navigate = useNavigate()
-    function handleClick(id){
+    function handleClick(id) {
         navigate(`/product/${id}`)
     }
     const displayProducts = searchTerm
         ? productList.filter((item) =>
-              item.title.toLowerCase().includes(searchTerm.toLowerCase())
-          )
+            item.title.toLowerCase().includes(searchTerm.toLowerCase())
+        )
         : productList;
-    if(displayProducts && displayProducts.length!==0){
+    if (displayProducts && displayProducts.length !== 0) {
         return (
-            <div className="grid grid-cols-2 gap-5 md:flex md:flex-wrap md:gap-10 lg:gap-7">
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
                 {
                     displayProducts.map((item) => (
-                        <div 
-                            key={item.id} 
+                        <div
+                            key={item.id}
                             onClick={() => handleClick(item.id)}
-                            className="w-full sm:w-[48%] md:w-[45%] lg:w-[30%] rounded p-4 bg-white shadow-[0px_0px_4px_0.5px_rgba(0,0,0,0.1)]">
+                            className="w-full rounded p-4 bg-white shadow-[0px_0px_4px_0.5px_rgba(0,0,0,0.1)]">
                             <div className="mb-1 md:mb-2 font-bold"><p>{item.title}</p></div>
-                            <div className="w-full sm:h-80 md:h-75 lg:h-64">
-                                <img 
-                                    src={item.image} 
-                                    alt={item.title} 
-                                    className="scale-100 w-full h-full object-cover"
-                                />
-                            </div>
+                            <img
+                                src={item.image}
+                                alt={item.title}
+                                className="scale-100 w-full h-50 md:h-75 object-cover"
+                            />
                             <div className="flex items-center mt-2 mx-1 w-[full] justify-between">
                                 <p className="text-sm text-black font-medium">₹{item.price}</p>
-                                <button onClick={()=>addToCart(item)} className=
-                                "p-2 bg-gray-500 hover:bg-black text-sm text-white w-[max] rounded cursor-pointer">
-                                Add to Cart</button>
+                                <button onClick={() => addToCart(item)} className=
+                                    "p-2 bg-gray-500 hover:bg-black text-sm text-white w-[max] rounded cursor-pointer">
+                                    Add to Cart</button>
                             </div>
                         </div>
                     ))
                 }
             </div>
         )
-    }else{
-        return(
+    } else {
+        return (
             <p className="font-bold">No matched Products!</p>
         )
     }
